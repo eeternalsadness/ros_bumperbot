fa7d1ec29b3d4ba0cd14481cd28af65ef9c37066
Bach Nguyen <69bnguyen@gmail.com>
2023-09-26 09:09:06 +0700

    transformation matrix c++

--- src/bumperbot_examples/src/turtlesim_kinematics.cpp
+++ src/bumperbot_examples/src/turtlesim_kinematics.cpp
@@ -15,8 +15,16 @@ void TurtlesimKinematics::turtle2PoseCallback(const turtlesim::Pose& pose){
     float Tx = last_turtle2_pose_.x - last_turtle1_pose_.x;
     float Ty = last_turtle2_pose_.y - last_turtle1_pose_.y;
 
+    float theta_rad = last_turtle2_pose_.theta - last_turtle1_pose_.theta;
+    float theta_deg = 180 * theta_rad / 3.14;
+
     ROS_INFO_STREAM("----------------\n" <<
                     "Translation Vector turtle1 -> turtle2 \n" <<
                     "Tx: " << Tx << "\n" <<
-                    "Ty: " << Ty << "\n");
+                    "Ty: " << Ty << "\n" <<
+                    "Rotation Matrix turtle1 -> turtle2\n" <<
+                    "theta (rad): " << theta_rad << "\n" <<
+                    "theta (deg): " << theta_deg << "\n" <<
+                    "| R11      R12 | : | " << std::cos(theta_rad) << "      " << -std::sin(theta_rad) << " |\n" <<
+                    "| R21      R22 |   | " << std::sin(theta_rad) << "      " << std::cos(theta_rad) << " |\n");
 }
