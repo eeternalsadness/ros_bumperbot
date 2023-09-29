27c0ef53eaaa60806a529d6e5fe4422554b99596
Bach Nguyen <69bnguyen@gmail.com>
2023-09-29 10:38:47 +0700

    working c++ simple controller

--- src/bumperbot_controller/nodes/simple_controller_node.cpp
+++ src/bumperbot_controller/nodes/simple_controller_node.cpp
@@ -1,16 +1,17 @@
 #include "bumperbot_controller/simple_controller.h"
 
 int main(int argc, char **argv){
-    ros::init(argc, argv, "simple_controller_node");
-    
+    ros::init(argc, argv, "simple_controller");
+
+    // need a node handle with a private namespace to access the private parameters
     ros::NodeHandle pnh("~");
     double wheel_radius;
-    pnh.getParam("~wheel_radius", wheel_radius);
+    pnh.getParam("wheel_radius", wheel_radius);
     double wheel_separation;
-    pnh.getParam("~wheel_separation", wheel_separation);
+    pnh.getParam("wheel_separation", wheel_separation);
 
-    ros::NodeHandle n;
-    SimpleController controller(n, wheel_radius, wheel_separation);
+    ros::NodeHandle nh;
+    SimpleController controller(nh, wheel_radius, wheel_separation);
 
     ros::spin();
 
--- src/bumperbot_controller/src/simple_controller.cpp
+++ src/bumperbot_controller/src/simple_controller.cpp
@@ -1,15 +1,15 @@
 #include "bumperbot_controller/simple_controller.h"
 #include <std_msgs/Float64.h>
+#include <Eigen/Geometry>
 
-SimpleController::SimpleController(const ros::NodeHandle &nh, double wheel_radius, double wheel_separation){
+SimpleController::SimpleController(const ros::NodeHandle &nh, double wheel_radius, double wheel_separation) : nh_(nh){
     ROS_INFO_STREAM("Using wheel radius " << wheel_radius);
     ROS_INFO_STREAM("Using wheel separation " << wheel_separation);
 
-    nh_ = nh;
     right_cmd_pub_ = nh_.advertise<std_msgs::Float64>("wheel_right_controller/command", 10);
     left_cmd_pub_ = nh_.advertise<std_msgs::Float64>("wheel_left_controller/command", 10);
 
-    vel_sub_ = nh_.subscribe("bumperbot_controller/cmd_vel", 10, &SimpleController::velCallback, this);
+    vel_sub_ = nh_.subscribe("bumperbot_controller/cmd_vel", 1000, &SimpleController::velCallback, this);
 
     speed_conversion_ << wheel_radius / 2, wheel_radius / 2,
                          wheel_radius / wheel_separation, -wheel_radius / wheel_separation;
@@ -18,15 +18,13 @@ SimpleController::SimpleController(const ros::NodeHandle &nh, double wheel_radiu
 }
 
 void SimpleController::velCallback(const geometry_msgs::Twist& msg){
-    Eigen::Matrix2d robot_speed;
-    robot_speed << msg.linear.x,
-                   msg.angular.z;
+    Eigen::Vector2d robot_speed(msg.linear.x, msg.angular.z);
     
-    Eigen::Matrix2d wheel_speed = speed_conversion_.inverse() * robot_speed;
+    Eigen::Vector2d wheel_speed = speed_conversion_.inverse() * robot_speed;
     std_msgs::Float64 right_speed;
-    right_speed.data = wheel_speed(0, 0);
+    right_speed.data = wheel_speed(0);
     std_msgs::Float64 left_speed;
-    left_speed.data = wheel_speed(1, 0);
+    left_speed.data = wheel_speed(1);
 
     right_cmd_pub_.publish(right_speed);
     left_cmd_pub_.publish(left_speed);
