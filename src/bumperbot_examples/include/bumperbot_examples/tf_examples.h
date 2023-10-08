e07f0a008f6e30e050180e1b9c4bf7320c6bee17
Bach Nguyen <69bnguyen@gmail.com>
2023-10-08 20:45:38 +0700

    c++ simple TF listener

--- src/bumperbot_examples/include/bumperbot_examples/tf_examples.h
+++ src/bumperbot_examples/include/bumperbot_examples/tf_examples.h
@@ -3,6 +3,8 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+#include "bumperbot_examples/GetTransform.h"
 
 class TfExamples {
 public:
@@ -15,8 +17,13 @@ private:
     ros::Timer timer_;
     double last_x_;
     double x_increment_;
+    ros::ServiceServer get_transform_srv_;
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 
     void timerCallback(const ros::TimerEvent &);
+    bool getTransformCallback(bumperbot_examples::GetTransform::Request &req,
+                              bumperbot_examples::GetTransform::Response &res);
 };
 
 #endif
--- src/bumperbot_examples/src/tf_examples.cpp
+++ src/bumperbot_examples/src/tf_examples.cpp
@@ -1,13 +1,14 @@
 // http://docs.ros.org/en/melodic/api/geometry_msgs/html/msg/TransformStamped.html
 // http://wiki.ros.org/tf2_ros?distro=noetic
 // https://wiki.ros.org/tf/Tutorials/Writing%20a%20tf%20broadcaster%20(C++)
+// http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20listener%20%28C%2B%2B%29
 
 #include "bumperbot_examples/tf_examples.h"
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 
 TfExamples::TfExamples(const ros::NodeHandle &nh) 
-    : nh_(nh), last_x_(0.0), x_increment_(0.05) {
+    : nh_(nh), last_x_(0.0), x_increment_(0.05), tf_listener_(tf_buffer_) {
     static tf2_ros::StaticTransformBroadcaster static_broadcaster;
 
     static_transform_stamped_.header.stamp = ros::Time::now();
@@ -28,6 +29,8 @@ TfExamples::TfExamples(const ros::NodeHandle &nh)
                     " and " << static_transform_stamped_.child_frame_id);
 
     timer_ = nh_.createTimer(ros::Duration(0.1), &TfExamples::timerCallback, this);
+
+    get_transform_srv_ = nh_.advertiseService("get_transform", &TfExamples::getTransformCallback, this);
 }
 
 void TfExamples::timerCallback(const ros::TimerEvent &event){
@@ -48,4 +51,26 @@ void TfExamples::timerCallback(const ros::TimerEvent &event){
 
     dynamic_broadcaster.sendTransform(dynamic_transform_stamped_);
     last_x_ = dynamic_transform_stamped_.transform.translation.x;
+}
+
+bool TfExamples::getTransformCallback(
+    bumperbot_examples::GetTransform::Request &req,
+    bumperbot_examples::GetTransform::Response &res){
+    ROS_INFO_STREAM("Requested transform between " << req.frame_id <<  " and " << req.child_frame_id);
+
+    geometry_msgs::TransformStamped requested_transform;
+
+    try{
+        requested_transform = tf_buffer_.lookupTransform(req.frame_id, req.child_frame_id, ros::Time(0));
+    }
+    catch(const tf2::TransformException &ex){
+        ROS_ERROR_STREAM("An error occurred while transforming " << req.frame_id << " and " << req.child_frame_id << ": " << ex.what());
+        res.success = false;
+        return true;
+    }
+
+    ROS_INFO_STREAM("The requested transform is:\n" << requested_transform);
+    res.transform = requested_transform;
+    res.success = true;
+    return true;
 }
